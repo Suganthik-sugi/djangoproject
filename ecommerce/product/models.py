16bc63dad2fc08b3eac7f302bbb398272aa5bf52
sugikrish <suginkl@gmail.com>
2023-07-11 22:26:33 +0530

    11

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class product(models.Model):
-  productname=models.CharField(max_length=20,unique=True)
+  productname=models.CharField(max_length=20)
   datecurrent=models.CharField(max_length=20)
   
 
