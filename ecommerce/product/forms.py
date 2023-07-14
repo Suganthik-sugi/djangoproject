2ab26b77b6a354eea032771110a3f3c6c5fcc15b
sugikrish <suginkl@gmail.com>
2023-07-14 18:57:36 +0530

    july14

--- ecommerce/product/forms.py
+++ ecommerce/product/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from .models import product
 
 class UniversityForm(forms.Form):
   name=forms.CharField(max_length=105, widget=forms.TextInput(attrs={'autofocus': True}),)
@@ -10,11 +11,14 @@ class UniversityForm(forms.Form):
   
   # date_of_birth=forms.DateField()
 
-class UniversityForm1(forms.Form):
+class UniversityForm1(forms.ModelForm):
+  class Meta:
+    model=product
+    fields=['productname']
   # name=forms.CharField(label='Product name')
   # date_of_birth=forms.DateField()
   # eid=forms.CharField(label='Product id:')
-  eid=forms.CharField(label='Product id:')
-  ename=forms.CharField(label='Product Name:')
+    eid=forms.CharField(label='Product id:')
+    ename=forms.CharField(label='Product Name:')
 
  
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -10,22 +10,43 @@ from crispy_forms.layout import Submit
 from datetime import datetime
 from django.contrib import messages
 from django.db.utils import IntegrityError
+from .models import product
 
 # Create your views here.
 
 
 def home(request):
 
-    if request.method == 'POST':
+    # if request.method == 'POST':
 
+    #     name = request.POST.get('name')
+    #     now = datetime.now()
+    #     cdate = now.strftime('%d-%m-%Y')
+    #     contact_data = product(productname=name, datecurrent=cdate)
+    #     contact_data.save()
+    #     return render(request, 'product/success.html')
+    # context={'form':UniversityForm}
+    # return render(request,'product/index2.html',context)
+
+  try:
+    if request.method == 'POST':
         name = request.POST.get('name')
         now = datetime.now()
         cdate = now.strftime('%d-%m-%Y')
         contact_data = product(productname=name, datecurrent=cdate)
         contact_data.save()
         return render(request, 'product/success.html')
-    context={'form':UniversityForm}
-    return render(request,'product/index2.html',context)
+    
+    context = {'form': UniversityForm}
+    return render(request, 'product/index2.html', context)
+
+  except Exception as e:
+    # Handle the exception here
+    # You can choose to display an error message to the user or perform any other desired action
+    error_message = str(e)  # Convert the exception to a string
+    context = {'error_message': error_message}
+    return render(request, 'product/error.html', context)
+
 
 
 def forminsert(request):
@@ -56,10 +77,16 @@ def delete(request,id):
     # pro_dict={'pro_list':mydata}
     # return render(request,'product/template.html',context=pro_dict)
 
-def edit(request,id):
-     employee = product.objects.get(id=id) 
-     pro_update={'product':employee,'form':UniversityForm1} 
-     return render(request, 'product/update.html',context=pro_update)
+# def edit(request,id):
+#      employee = product.objects.get(id=id) 
+#      pro_update={'product':employee,'form':UniversityForm1} 
+#      return render(request, 'product/update.html',context=pro_update)
 
-def IntegrityError(request,exception):
-    return render(request,'product/error.html')
+
+def edit(request, id):
+    employee = product.objects.get(id=id)
+    form = UniversityForm1(instance=employee)
+    return render(request, 'product/update.html', {'form': form})
+
+# def IntegrityError(request,exception):
+#     return render(request,'product/error.html')
